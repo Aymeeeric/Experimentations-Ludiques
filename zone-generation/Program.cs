0cba42d7abe5b6f5685a3b7381b68e76be4bde67
Aymeric <aymeric@recursif.dev>
2024-01-08 19:06:29 +0100

    Clean Fluent UI

--- zone-generation/Program.cs
+++ zone-generation/Program.cs
@@ -1,10 +1,13 @@
-using sector_generation.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+using zone_generation.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddFluentUIComponents();
 
 var app = builder.Build();
 
