9f80113129a4d017db0f3e66c3ad898ba6040d1f
Aymeric <aymeric@recursif.dev>
2024-01-14 11:09:56 +0100

    Début de mapping model / vue

--- Website/Components/Slices/MYZ/SectorsGeneration/Queries/GenerateSectorsQueryHandler.cs
+++ Website/Components/Slices/MYZ/SectorsGeneration/Queries/GenerateSectorsQueryHandler.cs
@@ -19,6 +19,8 @@ public class GenerateSectorsQueryHandler: IRequestHandler<GenerateSectorsQuery, 
     
     public async Task<List<Sector>> Handle(GenerateSectorsQuery query, CancellationToken cancellationToken)
     {
+        return GetMockedSectors();
+        
         var seed = query.Seed;
         var numberOfSectorsToGenerate = query.NumberOfSectorsToGenerate;
         var threatLevel = query.ThreatLevel;
@@ -42,6 +44,37 @@ public class GenerateSectorsQueryHandler: IRequestHandler<GenerateSectorsQuery, 
         return results;
     }
 
+    private List<Sector> GetMockedSectors()
+    {
+        var title = new SectorTitle("Ruines croulantes", 3, 1, GetGangreneColor(1));
+        var ruin = new SectorRuin(true, "normale", "Station de radio",
+            "une forêt d’antennes rouillées et de disques de métal surmonte le toit de ce vieux bâtiment. Un panneau sur la façade représente une antenne ceinte de quelques anneaux.");
+        var atmosphere = new SectorAtmosphere(true, "Message",
+            "quelqu’un a écrit quelque chose sur un mur. Quel est le message et est-il récent ?");
+        var threat = new SectorThreat(true, "Phénomène", "champ d'inertie", 184);
+        var artifact = new SectorArtifact(true, "Flacon de parfum", 193);
+        var trinket = new SectorTrinket(["Peigne", "Bible"]);
+
+        var sector = new Sector(title, ruin, atmosphere, threat, artifact, trinket);
+        return [sector];
+    }
+
+    private string GetGangreneColor(int level)
+    {
+        switch (level)
+        {
+            case 1 :
+                return "yellow";
+            case 2 :
+                return "orange";
+            case 3 :
+                return "red";
+            
+            default:
+                throw new Exception("Le niveau de gangrène doit être compris entre 1 et 3.");
+        }
+    }
+
     private void SeedRandomisation(string seed)
     {
         var intSeed = seed.GetHashCode();
@@ -51,13 +84,13 @@ public class GenerateSectorsQueryHandler: IRequestHandler<GenerateSectorsQuery, 
     private async Task LoadAllTables()
     {
         _tableEnvironnements = ConvertJsonFile<Environnement>("environnements.json");
-        _tableRuines = ConvertJsonFile<Ruine>("environnements.json");
-        _tableAmbiances = ConvertJsonFile<Ambiance>("environnements.json");
-        _tableMenacesHumanoides = ConvertJsonFile<Menace>("environnements.json");
-        _tableMenacesMonstres = ConvertJsonFile<Menace>("environnements.json");
-        _tableMenacesPhenomenes = ConvertJsonFile<Menace>("environnements.json");
-        _tableArtefacts = ConvertJsonFile<Artefact>("environnements.json");
-        _tableBabioles = ConvertJsonFile<Babiole>("environnements.json");
+        _tableRuines = ConvertJsonFile<Ruine>("ruines.json");
+        _tableAmbiances = ConvertJsonFile<Ambiance>("ambiances.json");
+        _tableMenacesHumanoides = ConvertJsonFile<Menace>("menaceHumanoides.json");
+        _tableMenacesMonstres = ConvertJsonFile<Menace>("menacesMonstres.json");
+        _tableMenacesPhenomenes = ConvertJsonFile<Menace>("menacesPhenomenes.json");
+        _tableArtefacts = ConvertJsonFile<Artefact>("artefacts.json");
+        _tableBabioles = ConvertJsonFile<Babiole>("babioles.json");
     }
 
     private List<T> ConvertJsonFile<T>(string environnementsJson)
@@ -76,7 +109,6 @@ public enum GenerationMode {
     Random
 }
 
-public record Sector();
 public record Environnement();
 public record Ruine();
 public record Ambiance();
---
+++ Website/Components/Slices/MYZ/SectorsGeneration/Queries/Sector.cs
@@ -0,0 +1,10 @@
+﻿namespace Aymeeeric.Website.Components.Slices.MYZ.SectorsGeneration.Queries;
+
+public record Sector(SectorTitle Title, SectorRuin Ruin, SectorAtmosphere Atmosphere, SectorThreat Threat, SectorArtifact Artifact, SectorTrinket Trinket);
+
+public record SectorTitle(string Environment, int ThreatLevel, int GangreneLevel, string GangreneColor);
+public record SectorRuin(bool IsRuinInSector, string RuinType, string RuinName, string RuinDefinition);
+public record SectorAtmosphere(bool IsAtmosphereInSector, string AtmosphereName, string AtmosphereDefinition);
+public record SectorThreat(bool IsThreatInSector, string ThreatType, string ThreatName, int ThreatPage);
+public record SectorArtifact(bool IsArtifactInSector, string ArtifactName, int ArtifactPage);
+public record SectorTrinket(List<string> TrinketsNames);
