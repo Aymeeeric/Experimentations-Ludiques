4de2603e25af01f5424505d65c69541480ab9bb2
Aymeric <aymeric@recursif.dev>
2024-01-28 16:07:51 +0100

    Correction Heroku Port mapping

--- Website/Program.cs
+++ Website/Program.cs
@@ -3,6 +3,13 @@ using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Spécificité pour tourner sur Heroku...
+builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+{
+    var herokuPort = int.Parse(Environment.GetEnvironmentVariable("PORT") ?? throw new Exception("Impossible de trouver le port de lancement."));
+    serverOptions.ListenAnyIP(herokuPort);
+});
+
 // Add services to the container.
 builder.Services
     .AddRazorComponents()
